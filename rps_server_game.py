f6ce2eb36cff3dc446111f116a8e96e1107752bb
Omar <omarehap17@gmail.com>
2023-05-21 20:42:40 +0300

    fixes

--- rps_server_game.py
+++ rps_server_game.py
@@ -8,8 +8,6 @@ GAME_RULES = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
 # Players waiting to play
 waiting_players = deque(maxlen=6)
 
-# Active games
-games = {}
 
 class Player:
     def __init__(self, conn, addr):
@@ -21,22 +19,26 @@ def handle_client(player):
     """Handle client connection"""
     while True:
         try:
-            msg = player.conn.recv(1024).decode('utf-8')
+            msg = player.conn.recv(1024).decode('utf-8').strip()
             if msg in GAME_RULES.keys():
                 player.choice = msg
                 play_game(player)
             elif msg == 'quit':
                 player.conn.close()
+                waiting_players.remove(player)
+                broadcast_waiting_list()
                 break
         except:
             print('Connection closed')
+            waiting_players.remove(player)
+            broadcast_waiting_list()
             break
 
 def play_game(player):
     """Handle game logic"""
     if len(waiting_players) < 2:
         waiting_players.append(player)
-        broadcast_waiting_list()
+        broadcast_waiting_list(waiting_players)
     else:
         player1 = waiting_players.popleft()
         player2 = waiting_players.popleft() if waiting_players else player
@@ -48,12 +50,13 @@ def play_game(player):
 def determine_winner(player1, player2):
     """Determine game winner"""
     if GAME_RULES[player1.choice] == player2.choice:
-        return player1, player1.choice, player2.choice
+        return player1
     elif GAME_RULES[player2.choice] == player1.choice:
-        return player2, player1.choice, player2.choice
+        return player2
+    else:
+        return None
 
-
-def broadcast_waiting_list():
+def broadcast_waiting_list(waiting_players):
     """Broadcast waiting list to all clients"""
     for player in waiting_players:
         player.conn.send(f'Waiting players: {[p.addr for p in waiting_players]}\n'.encode('utf-8'))
--- test_rps1.py
+++ test_rps1.py
@@ -10,24 +10,18 @@ class TestRPS(unittest.TestCase):
 
         player1.choice = 'rock'
         player2.choice = 'scissors'
-        winner, choice1, choice2 = determine_winner(player1, player2)
+        winner= determine_winner(player1, player2)
         self.assertEqual(winner, player1)
-        self.assertEqual(choice1, 'rock')
-        self.assertEqual(choice2, 'scissors')
 
         player1.choice = 'rock'
         player2.choice = 'paper'
-        winner, choice1, choice2 = determine_winner(player1, player2)
+        winner = determine_winner(player1, player2)
         self.assertEqual(winner, player2)
-        self.assertEqual(choice1, 'rock')
-        self.assertEqual(choice2, 'paper')
 
         player1.choice = 'scissors'
         player2.choice = 'paper'
-        winner, choice1, choice2 = determine_winner(player1, player2)
+        winner = determine_winner(player1, player2)
         self.assertEqual(winner, player1)
-        self.assertEqual(choice1, 'scissors')
-        self.assertEqual(choice2, 'paper')
 
 if __name__ == '__main__':
     unittest.main()
